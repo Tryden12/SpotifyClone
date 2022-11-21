996e06aa258952bd69bfc799745e014a8ba4454a
Tyler Ryden <tryden11@gmail.com>
2022-11-20 20:58:27 -0600

    dagger & hilt setup, gradle changes to fix deprecation and update versions

---
+++ app/src/main/java/com/tryden/spotifyclone/di/ServiceModule.kt
@@ -0,0 +1,44 @@
+@file:Suppress("DEPRECATION")
+
+package com.tryden.spotifyclone.di
+
+import android.content.Context
+import com.google.android.exoplayer2.C
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.audio.AudioAttributes
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Util
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ServiceComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.scopes.ServiceScoped
+
+@Module
+@InstallIn(ServiceComponent::class)
+object ServiceModule {
+
+    @ServiceScoped
+    @Provides
+    fun provideAudioAttributes() = AudioAttributes.Builder()
+        .setContentType(C.AUDIO_CONTENT_TYPE_MUSIC)
+        .setUsage(C.USAGE_MEDIA)
+        .build()
+
+    @ServiceScoped
+    @Provides
+    fun provideExoPlayer(
+        @ApplicationContext context: Context,
+        audioAttributes: AudioAttributes
+    ) = SimpleExoPlayer.Builder(context).build().apply {
+        setAudioAttributes(audioAttributes, true) // apply audio attributes to ExoPlayer
+        setHandleAudioBecomingNoisy(true) // Pause music if headphones plugged in or unplugged
+    }
+
+    @ServiceScoped
+    @Provides
+    fun provideDataSourceFactory( // provides our firebase song data
+        @ApplicationContext context: Context
+    ) = DefaultDataSourceFactory(context, Util.getUserAgent(context, "Spotify App"))
+}
