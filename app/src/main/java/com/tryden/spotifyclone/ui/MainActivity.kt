81a3b50c3f50ac6018f7e73970022e8da13c838d
Tyler Ryden <tryden11@gmail.com>
2022-11-30 16:17:50 -0600

    setup main view model

--- app/src/main/java/com/tryden/spotifyclone/ui/MainActivity.kt
+++ app/src/main/java/com/tryden/spotifyclone/ui/MainActivity.kt
@@ -1,8 +1,9 @@
-package com.tryden.spotifyclone
+package com.tryden.spotifyclone.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bumptech.glide.RequestManager
+import com.tryden.spotifyclone.R
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
